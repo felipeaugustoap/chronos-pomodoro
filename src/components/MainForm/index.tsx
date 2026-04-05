2c22544a673d7640ddf3806f07579405ed1fa9b4
Felipe Augusto <126160371+felipeaugustoap@users.noreply.github.com>
2026-04-05 13:53:55 +0200

    I created pages AboutPomodoro, NotFound, Home, components GenericHtml, modified MainForm

---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,33 @@
+import { PlayCircleIcon } from "lucide-react"
+import { Cycles } from "../Cycles"
+import { DefaultButton } from "../DefaultButton"
+import { DefaultInput } from "../DefaultInput"
+import styles from "./styles.module.css"
+
+
+
+export function MainForm () {
+    
+    return <form className={styles.form} action="">
+                    <div className={styles.formRow}>
+                        <DefaultInput
+                            labelText="task"
+                            type="text"
+                            id='meuInput'
+                            placeholder='Digite algo'
+                        />
+                    </div>
+
+                    <div className={styles.formRow}>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className={styles.formRow}>
+                        <Cycles/>
+                    </div>
+
+                    <div className={styles.formRow}>
+                        <DefaultButton icon={<PlayCircleIcon />} color='green' />
+                    </div>
+                </form>
+}
