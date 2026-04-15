05306d19d464c1a158499c3761c45e00175c5846
Felipe Augusto <126160371+felipeaugustoap@users.noreply.github.com>
2026-04-15 07:22:49 +0200

    Created function convert seconds to minutes

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,6 +8,7 @@ import type { TaskModel } from "../../models/TaskModel"
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
 import { getNextCycle } from "../../utils/getNextCycle"
 import { getNextCycleType } from "../../utils/getNextCycleType"
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes"
 
 
 
@@ -40,7 +41,7 @@ export function MainForm() {
             startDate: Date.now(),
             completeDate: null,
             interruptDate: null,
-            duration: 1,
+            duration: state.config[nextCycleType],
             type: nextCycleType,
         };
 
@@ -53,7 +54,7 @@ export function MainForm() {
                 activeTask: newTask,
                 currentCycle: nextCycle,
                 secondsRemaining,
-                formattedSecondsRemaining: "00:00",
+                formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
                 tasks:[...prevState.tasks, newTask],
             }
         })
