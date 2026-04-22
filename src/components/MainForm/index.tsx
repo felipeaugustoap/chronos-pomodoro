f45579e69e1deda52c210c36e89eab5339b60ac9
Felipe Augusto <126160371+felipeaugustoap@users.noreply.github.com>
2026-04-22 08:03:40 +0200

    Starting implementation of Reducer

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,12 +8,12 @@ import type { TaskModel } from "../../models/TaskModel"
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
 import { getNextCycle } from "../../utils/getNextCycle"
 import { getNextCycleType } from "../../utils/getNextCycleType"
-import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes"
+//import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes"
 
 
 
 export function MainForm() {
-    const { state, setState } = useTaskContext()
+    const { state, dispatch } = useTaskContext()
     
     //ciclos
 
@@ -47,29 +47,37 @@ export function MainForm() {
 
         const secondsRemaining = newTask.duration * 60
 
-        setState(prevState => {
-            return {
-                ...prevState,
-                config: {...prevState.config},
-                activeTask: newTask,
-                currentCycle: nextCycle,
-                secondsRemaining,
-                formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
-                tasks:[...prevState.tasks, newTask],
-            }
-        })
+        // setState(prevState => {
+        //     return {
+        //         ...prevState,
+        //         config: {...prevState.config},
+        //         activeTask: newTask,
+        //         currentCycle: nextCycle,
+        //         secondsRemaining,
+        //         formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        //         tasks:[...prevState.tasks, newTask],
+        //     }
+        // })
     };
 
 
     function handleInterruptTask() {
-        setState(prevState => {
-            return {
-                ...prevState,
-                activeTask: null,
-                secondsRemaining: 0,
-                formattedSecondsRemaining: "00:00",
-            }
-        })
+        // setState(prevState => {
+        //     return {
+        //         ...prevState,
+        //         activeTask: null,
+        //         secondsRemaining: 0,
+        //         formattedSecondsRemaining: "00:00",
+        //         tasks: prevState.tasks.map(task => {
+        //             if (prevState.activeTask && prevState.activeTask.id === task.id) {
+        //                 return {
+        //                     ...task, interruptDate: Date.now()
+        //                 }
+        //             }
+        //             return task
+        //         })
+        //     }
+        // })
     }
     
     return (
