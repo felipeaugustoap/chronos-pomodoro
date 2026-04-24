d1d300b7d94d558a0f292ba2005dee4fd80d3939
Felipe Augusto <126160371+felipeaugustoap@users.noreply.github.com>
2026-04-24 07:56:42 +0200

    Continue Implementing Reducer

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,7 +8,7 @@ import type { TaskModel } from "../../models/TaskModel"
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
 import { getNextCycle } from "../../utils/getNextCycle"
 import { getNextCycleType } from "../../utils/getNextCycleType"
-//import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes"
+import { TaskActionTypes } from "../../contexts/TaskContext/taskActions"
 
 
 
@@ -45,39 +45,13 @@ export function MainForm() {
             type: nextCycleType,
         };
 
-        const secondsRemaining = newTask.duration * 60
-
-        // setState(prevState => {
-        //     return {
-        //         ...prevState,
-        //         config: {...prevState.config},
-        //         activeTask: newTask,
-        //         currentCycle: nextCycle,
-        //         secondsRemaining,
-        //         formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
-        //         tasks:[...prevState.tasks, newTask],
-        //     }
-        // })
+        dispatch({type: TaskActionTypes.START_TASK, payload: newTask})
+        
     };
 
 
     function handleInterruptTask() {
-        // setState(prevState => {
-        //     return {
-        //         ...prevState,
-        //         activeTask: null,
-        //         secondsRemaining: 0,
-        //         formattedSecondsRemaining: "00:00",
-        //         tasks: prevState.tasks.map(task => {
-        //             if (prevState.activeTask && prevState.activeTask.id === task.id) {
-        //                 return {
-        //                     ...task, interruptDate: Date.now()
-        //                 }
-        //             }
-        //             return task
-        //         })
-        //     }
-        // })
+        dispatch({type: TaskActionTypes.INTERRUPT_TASK})
     }
     
     return (
