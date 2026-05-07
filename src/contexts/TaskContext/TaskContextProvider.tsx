079b3033192a2cad5475bba4eccbdc6fa678c947
Felipe Augusto <126160371+felipeaugustoap@users.noreply.github.com>
2026-05-07 07:53:38 +0200

    Inserting beep audio in Provider

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
 import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
 import { TaskActionTypes } from "./taskActions";
+import { loadBeep } from "../../utils/loadBeep";
 
 
 
@@ -13,16 +14,21 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
     const [state, dispatch] = useReducer(taskReducer, initialTaskState)
+    const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
     const worker = TimerWorkerManager.getInstance()
 
+    // eslint-disable-next-line react-hooks/refs
     worker.onmessage(e => {
         const countDownSeconds = e.data
-        console.log(countDownSeconds)
-
-        
 
         if (countDownSeconds <= 0) {
+
+            if (playBeepRef.current) {
+                playBeepRef.current()
+                playBeepRef.current = null;
+            };
+            
             dispatch({
                 type: TaskActionTypes.COMPLETE_TASK,
             })
@@ -34,16 +40,24 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
             })
         }
     })
+    
 
 
     useEffect(() => {
         if (!state.activeTask) {
-            console.log("Worker terminado por falta de activeTask")
             worker.terminate()
         }
 
         worker.postMessage(state)
-    }, [worker,state])
+    }, [worker, state])
+    
+    useEffect(() => {
+        if (state.activeTask && playBeepRef.current === null) {
+            playBeepRef.current = loadBeep()
+        } else {
+            playBeepRef.current = null;
+        }
+    }, [state.activeTask])
 
     return (
         <TaskContext.Provider value={{state, dispatch}}>
